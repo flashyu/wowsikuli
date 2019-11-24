6aafe6dd2367ccee42730adf565ee9090bd9621c
yupeng <flashyupeng@gmail.com>
2019-11-25 00:26:51 +0800

    u

--- guaji.sikuli/guaji.py
+++ guaji.sikuli/guaji.py
@@ -1,20 +1,25 @@
+def gameStart():
+    exists("1574096724785.png", 60);
+    click("1574096724785.png", 20);
+    exists("1574096829952.png",60);
+    return;
+
 def login():
     print 'login';
     if not exists("1574095716601.png"):
         print 'no login icon';
         openApp('D:\Battle.net\Battle.net.exe');
     print 'login icon';
-    wait("1574095716601.png", 60);
-    click("1574095716601.png",20);
-    wait("1574096724785.png", 60);
-    click("1574096724785.png",20);
-    wait("1574096829952.png",60);
+    exists("1574095716601.png", 60);
+    click("1574095716601.png", 20);
+    exists("1574096724785.png", 60);
     print 'login success';
     return;
 
 def logout():
     print 'logout';
-    clcik(Pattern("1574095824831.png").targetOffset(2,37),3);
+    click("1574095824831.png");
+    sleep(1);
     type(Key.ESC);
     sleep(1);
     type(Key.ESC);
@@ -28,9 +33,14 @@ def guaji():
     return;
     
 while True:
-    if exists("1574096829952.png",3):
+    if exists("1574096829952.png", 3):
         guaji();
-    elif exists("1574095824831.png",3):
+    elif exists("1574096724785.png", 3):
+        gameStart();
+    elif exists("1574095824831.png", 3):
         logout();
+    elif exists("1574596348467.png", 3):
+        sleep(3);
+        print('in the line...');
     else:
         login();
