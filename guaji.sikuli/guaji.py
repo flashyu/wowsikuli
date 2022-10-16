17105b26cc9fdb73b94f5dc11611f7c35a3b4b66
yupeng07 <yupeng07@baidu.com>
2022-10-16 12:53:18 +0800

    优化自动重连

--- guaji.sikuli/guaji.py
+++ guaji.sikuli/guaji.py
@@ -1,18 +1,23 @@
 def gameStart():
+    print 'game start';
     click("1574096724785.png", 20);
-    exists("1574096829952.png",60);
+    sleep(60);
     return;
 
 def login():
     print 'login';
-    if not exists("1574095716601.png"):
+    if not exists("1574095716601.png", 10):
         print 'no login icon';
         openApp('D:\wow\Battle.net\Battle.net.exe');
-    print 'login icon';
-    exists("1574095716601.png", 60);
+        sleep(20);
     click("1574095716601.png", 20);
-    exists("1574096724785.png", 60);
     print 'login success';
+    return;
+
+def reconnect():
+    print 'reconnect';
+    click("1665894407351.png", 5);
+    sleep(20);
     return;
 
 def logout():
@@ -28,18 +33,20 @@ def logout():
 def guaji():
     print 'run';
     type(Key.SPACE);
-    sleep(5);
+    sleep(10);
     return;
     
 while True:
     if exists("1574096829952.png", 3):
         guaji();
-    elif exists("1574096724785.png", 3):
-        gameStart();
-    elif exists("1574095824831.png", 3):
-        logout();
-    elif exists("1574596348467.png", 3):
-        sleep(3);
+    elif exists("1574596348467.png", 5):
+        sleep(30);
         print('in the line...');
+    elif exists("1574096724785.png", 5):
+        gameStart();
+    elif exists("1574095824831.png", 5):
+        logout();
+    elif exists("1665894407351.png", 5):
+        reconnect();
     else:
         login();
