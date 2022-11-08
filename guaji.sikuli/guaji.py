aeed9b6ffdf9bd934021b0da97d79d70dfe43db8
yupeng07 <yupeng07@baidu.com>
2022-11-09 01:45:12 +0800

    1

--- guaji.sikuli/guaji.py
+++ guaji.sikuli/guaji.py
@@ -24,6 +24,8 @@ def logout():
     type(Key.ESC);
     sleep(1);
     type(Key.ESC);
+    sleep(1);
+    type(Key.ESC);
     sleep(3);
     return;
 
@@ -32,18 +34,25 @@ def guaji():
     type(Key.SPACE);
     sleep(10);
     return;
-    
+
 while True:
     if exists("1574096829952.png", 5):
         guaji();
     elif exists("1574096724785.png", 5):
         gameStart();
-    elif exists("1574095824831.png", 5):
-        logout();
+    elif exists("1667632021664.png", 5):
+        click("1667632021664.png");
+        sleep(3);
+    elif exists("1667929312740.png", 5):
+        click("1667929312740.png",5);
+        sleep(3);
+    elif exists("1667632055950.png", 5):
+        print 'rl';
+        click("1667632055950.png");
+        sleep(10);
     elif exists("1665894407351.png", 5):
         reconnect();
     elif exists("1574095716601.png", 5):
         login();
     else:
-        sleep(30);
-        print 'go on';
+        sleep(10);
